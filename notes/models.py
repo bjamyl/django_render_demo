27027940a940c692433233ed3dc4ad89c463eb89
bjamyl <alhassanjamil0@gmail.com>
2022-12-29 17:23:17 +0000

    created note model and made database migrations, installed and registered DRF

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(unique=True, max_length=200)
+    body = models.TextField()
+    time_updated = models.DateTimeField(auto_now=True)
+    time_created = models.DateTimeField(auto_now_add=True)
